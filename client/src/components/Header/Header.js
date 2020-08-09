77c5969db17ae710cd4ca507812fcd37d8878b58
Esther Min <estherjmin@gmail.com>
2020-08-09 10:15:46 -0400

    setup navbar component with reactstrap

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavBar from "../NavBar/NavBar";
+
+const Header = () => {
+    return (
+        <NavBar />
+    )
+}
+
+export default Header;
---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+const NavBar = () => {
+    return (
+        <Navbar color="faded" light expand="xs">
+        <NavbarBrand href="/" className="mr-auto">Google Books</NavbarBrand>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/components/">Search</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">Saved</NavLink>
+            </NavItem>
+          </Nav>
+      </Navbar>
+    )
+}
+
+export default NavBar;
