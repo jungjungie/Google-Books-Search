63cd585aa55f7108ca15807147fbdb071b68dd0c
Esther Min <estherjmin@gmail.com>
2020-08-09 13:26:12 -0400

    added header component

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
+import { Container } from 'reactstrap';
+import "./Header.css";
 
 const Header = () => {
     return (
-        <NavBar />
+        <header>
+            <NavBar />
+            <Container className="text-center">
+                <section id="header-section">
+                    <h1>Google Books Search</h1>
+                    <h4>Search for and Save Books of Interest!</h4>
+                </section>
+            </Container>
+        </header>
     )
 }
 
