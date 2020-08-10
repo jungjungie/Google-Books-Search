e8f6e4bd243e8c69755bd8f027917c8dd52143d0
Esther Min <estherjmin@gmail.com>
2020-08-09 22:34:43 -0400

    created book model and setup mongodb

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,12 @@ import axios from "axios";
 
 export default {
     // Gets the book with the searched title
-    getBook: function(book) {
+    getBook: function (book) {
         return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book);
+    },
+
+    // Saves a book to the database
+    saveBook: function (bookData) {
+        return axios.post("/api/books", bookData);
     }
 };
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const path = require("path");
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 // Define middleware here
@@ -18,6 +22,9 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books-search");
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
